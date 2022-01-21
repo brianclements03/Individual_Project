d993bc5fb3813a5f6a22480f4dfbc81a3726af37
Brian Clements <brian.l.clements03@gmail.com>
2022-01-21 15:05:50 -0600

    comment hypothesis testing

--- hypothesis_testing.py
+++ hypothesis_testing.py
@@ -25,8 +25,12 @@ import viz
 
 
 def levene1(df):
-    # a levene test of variance for depth by approval time
+    '''
+    Function to run a levene test of variance for depth by approval time
+    '''
+    # assign the test to a variable
     x = stats.levene(
+    # define the subgroups to be tested
     df[df.Approval_time_days==0].Total_Depth,
     df[df.Approval_time_days==1].Total_Depth,
     df[df.Approval_time_days==2].Total_Depth,
@@ -56,11 +60,14 @@ def levene1(df):
     df[df.Approval_time_days==26].Total_Depth,
     df[df.Approval_time_days==27].Total_Depth,
     )
+    # return x for calling in the jupyter notebook
     return x
 
 def dist_depth_by_approval_time1(df):
-    ''' plot the distributions of depth by approval time, to visually check they are normally distributed'''
-
+    ''' 
+    plot the distributions of depth by approval time, to visually check they are normally distributed
+    '''
+    # for loop to draw a histogram of each of the groups being tested:
     for i in [df[df.Approval_time_days==0].Total_Depth,
     df[df.Approval_time_days==1].Total_Depth,
     df[df.Approval_time_days==2].Total_Depth,
@@ -90,13 +97,16 @@ def dist_depth_by_approval_time1(df):
     df[df.Approval_time_days==26].Total_Depth,
     df[df.Approval_time_days==27].Total_Depth]:
         plt.figure(figsize=(4,3))
+        # the histogram being called for every variable i in the list in the for loop
         i.hist()
         plt.show()
 
 
 def kruskal_wallace1(df):
     '''function to run kw test on approval time by depth'''
+    # assign the results of the kruskal test to f and p variables
     f, p = stats.kruskal(
+    # list of all the groups being tested
     df[df.Approval_time_days==0].Total_Depth,
     df[df.Approval_time_days==1].Total_Depth,
     df[df.Approval_time_days==2].Total_Depth,
@@ -126,11 +136,13 @@ def kruskal_wallace1(df):
     df[df.Approval_time_days==26].Total_Depth,
     df[df.Approval_time_days==27].Total_Depth
     )
+    # return f and p to be called in the notebook
     return f, p
 
 
 def levene2(df):
     '''levene variance test like that run in an above cell:'''
+    # same as the above levene test: assign it to x and enter the subgroups being compared for variance
     x = stats.levene(
     df[df.SHALE == 'PERMIAN BASIN'].Approval_time_days,
     df[df.SHALE == 'EAGLE FORD'].Approval_time_days,
@@ -138,21 +150,26 @@ def levene2(df):
     df[df.SHALE == 'BARNETT'].Approval_time_days,
     df[df.SHALE == 'HAYNESVILLE'].Approval_time_days
     )
+    # return x to call on in the notebook
     return x
 
 def anova1(df):
-    # running an anova test on the approval times by shale
+    ''' 
+    Function to runn an anova test on the approval times by shale
+    '''
+    # assing the results of the anova to f and p, and enter the subgroups being analyzed
     f, p =stats.f_oneway(df[df.SHALE == 'PERMIAN BASIN'].Approval_time_days,
     df[df.SHALE == 'EAGLE FORD'].Approval_time_days,
     df[df.SHALE == 'NONE'].Approval_time_days,
     df[df.SHALE == 'BARNETT'].Approval_time_days,
     df[df.SHALE == 'HAYNESVILLE'].Approval_time_days)
-
+    # return f and p to call in the notebook
     return f,p
 
 
 def levene3(df):
     '''levene variance test like that run in an above cell:'''
+    # same as the above levene tests: assign it to x and enter the subgroups being compared for variance
     x = stats.levene(
     df[df.District == '08'].Approval_time_days,
     df[df.District == '01'].Approval_time_days,
@@ -167,11 +184,17 @@ def levene3(df):
     df[df.District == '10'].Approval_time_days,
     df[df.District == '05'].Approval_time_days
     )
+    # return x to call in notebook
     return x
 
 
 def viz_district_approvals_distributions(df):
+    '''
+    Function to visualize the distributions (i.e. histograms) of the variable being stats-tested
+    '''
+    # create figure and define its size
     plt.figure(figsize=(6,4))
+    # for loop to draw the histogram of each subgroup
     for i in [df[df.District == '08'].Approval_time_days,
         df[df.District == '01'].Approval_time_days,
         df[df.District == '02'].Approval_time_days,
@@ -184,10 +207,12 @@ def viz_district_approvals_distributions(df):
         df[df.District == '7B'].Approval_time_days,
         df[df.District == '10'].Approval_time_days,
         df[df.District == '05'].Approval_time_days]:
+        # draw a histogram for each i in the for loop
         i.hist()
 
 def anova2(df):
     '''running an anova test on the approval times by district'''
+     # assign the results of the anova to f and p, and enter the subgroups being analyzed
     f, p =stats.f_oneway(
     df[df.District == '08'].Approval_time_days,
     df[df.District == '01'].Approval_time_days,
@@ -202,13 +227,21 @@ def anova2(df):
     df[df.District == '10'].Approval_time_days,
     df[df.District == '05'].Approval_time_days
     )
-
+    # return f and p to call on in notebook
     return f,p
 
 
 def depth_and_approval_time(df):
+    '''
+    Function to print the average approval time for every depth bin and the overall average
+    '''
+    # define depth_bins as a list
     depth_bins = ['Shallow', 'Deep', 'Mid_depth']
+    # create the for loop, looping through the depth bins
     for i in depth_bins:
+        # define an output to call on in the print function below
         output = df[df.Depth_bin == i].Approval_time_days.mean()
+        # print function to add a little text and automation the the result
         print(f'{i} well avg approval time is {output}')
+    # add a statemen of the overall average approval time, outside the for loop
     print(f'Overall avg approval time is {df.Approval_time_days.mean()}')
